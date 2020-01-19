8e16852dd240bf2704ed9e5615a7d0af89181694
Justus Luthy <jluthy@coinlion.com>
2020-01-19 06:45:06 -0600

    uncomment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,58 +3,56 @@
 set -o errexit
 set -o pipefail
 
-env
+package() {
+    helm init --client-only
+    helm lint ${CHART}
+    mkdir -p /home/runner/pkg
+    helm package ${CHART} --destination /home/runner/pkg/
+}
 
-# package() {
-#     helm init --client-only
-#     helm lint ${CHART}
-#     mkdir -p /home/runner/pkg
-#     helm package ${CHART} --destination /home/runner/pkg/
-# }
+push() {
+  git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
+  git config user.name ${GITHUB_ACTOR}
+  git remote set-url origin ${REPOSITORY}
+  git checkout gh-pages
+  mv /home/runner/pkg/*.tgz .
+  helm repo index . --url ${URL}
+  git add .
+  git commit -m "Publish Helm chart ${CHART} ${TAG}"
+  git push origin gh-pages
+}
 
-# push() {
-#   git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
-#   git config user.name ${GITHUB_ACTOR}
-#   git remote set-url origin ${REPOSITORY}
-#   git checkout gh-pages
-#   mv /home/runner/pkg/*.tgz .
-#   helm repo index . --url ${URL}
-#   git add .
-#   git commit -m "Publish Helm chart ${CHART} ${TAG}"
-#   git push origin gh-pages
-# }
+REPOSITORY="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-# REPOSITORY="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+CHART=${INPUT_CHART_PATH}
+if [[ -z $1 ]] ; then
+  echo "Chart path parameter needed!" && exit 1;
+fi
 
-# CHART=${INPUT_CHART_PATH}
-# if [[ -z $1 ]] ; then
-#   echo "Chart path parameter needed!" && exit 1;
-# fi
+URL=${INPUT_GH_PAGES_URL}
+if [[ -z $2 ]] ; then
+  echo "Helm repository URL parameter needed!" && exit 1;
+fi
 
-# URL=${INPUT_GH_PAGES_URL}
-# if [[ -z $2 ]] ; then
-#   echo "Helm repository URL parameter needed!" && exit 1;
-# fi
+TAG=$(echo ${GITHUB_REF} | rev | cut -d/ -f1 | rev)
+if [[ "${GITHUB_REF}" == "refs/tags"* ]]; then
+    echo "Starting action for tag ${TAG}";
+else
+    echo "Skipping action because push does not refer to a git tag!" && exit 78;
+fi
 
-# TAG=$(echo ${GITHUB_REF} | rev | cut -d/ -f1 | rev)
-# if [[ "${GITHUB_REF}" == "refs/tags"* ]]; then
-#     echo "Starting action for tag ${TAG}";
-# else
-#     echo "Skipping action because push does not refer to a git tag!" && exit 78;
-# fi
+TAG_FILTER=${INPUT_TAG_FILTER}
+if [[ -z $3 ]]; then
+  echo "Tag filter not specified";
+else
+    if [[ ${TAG} != *${TAG_FILTER}* ]]; then
+    echo "Tag ${TAG} does not match filter ${TAG_FILTER}" && exit 78;
+    fi
+fi
 
-# TAG_FILTER=${INPUT_TAG_FILTER}
-# if [[ -z $3 ]]; then
-#   echo "Tag filter not specified";
-# else
-#     if [[ ${TAG} != *${TAG_FILTER}* ]]; then
-#     echo "Tag ${TAG} does not match filter ${TAG_FILTER}" && exit 78;
-#     fi
-# fi
+echo "Chart: ${CHART}";
+echo "URL: ${URL}";
+echo "Filter: ${TAG_FILTER}";
 
-# echo "Chart: ${CHART}";
-# echo "URL: ${URL}";
-# echo "Filter: ${TAG_FILTER}";
-
-# package
-# push
+package
+push
